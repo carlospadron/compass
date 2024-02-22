9b6e8d8079e651c88f82a891a8e23ed61fd0c417
carlospadron <padron.ca@gmail.com>
2024-02-22 21:26:21 +0000

    more methods for coordinates

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -81,6 +81,36 @@ impl Cooordinate {
         }
     }
 
+    /// Returns the ordinate value of the coordinate.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - The coordinate.
+    /// * `ordinate` - The index of the ordinate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use geoms::coordinate::Cooordinate;
+    ///
+    /// let coordinate = Cooordinate::D2 { x: 1.0, y: 2.0 };
+    /// assert_eq!(coordinate.get_ordinate(0), 1.0);
+    /// assert_eq!(coordinate.get_ordinate(1), 2.0);
+    ///
+    /// let coordinate = Cooordinate::D3 { x: 3.0, y: 4.0, z: 5.0 };
+    /// assert_eq!(coordinate.get_ordinate(0), 3.0);
+    /// assert_eq!(coordinate.get_ordinate(1), 4.0);
+    /// assert_eq!(coordinate.get_ordinate(2), 5.0);
+    /// ```
+    pub fn get_ordinate(&self, ordinate: usize) -> f64 {
+        match ordinate {
+            0 => self.x(),
+            1 => self.y(),
+            2 => self.z(),
+            _ => panic!("Invalid ordinate index")
+        }
+    }
+
     /// Checks if the coordinate values are valid, meaning they are finite.
     ///
     /// # Arguments
@@ -110,6 +140,7 @@ impl Cooordinate {
             Cooordinate::D3 { x, y, z } => x.is_finite() && y.is_finite() && z.is_finite()
         }
     }
+
     /// Returns whether the planar projections of the two coordinates are equal.
     ///
     /// # Arguments
@@ -136,6 +167,60 @@ impl Cooordinate {
     /// ```
     pub fn equals_2d(&self, other: &Cooordinate) -> bool {
         self.x() == other.x() && self.y() == other.y()
+    }
+    
+    /// Returns whether the planar projections of the two coordinates are equal within a given tolerance.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - The first coordinate.
+    /// * `other` - The second coordinate.
+    /// * `tolerance` - The maximum allowed difference between the coordinates' values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use geoms::coordinate::Cooordinate;
+    ///
+    /// let coordinate1 = Cooordinate::D2 { x: 1.0, y: 2.0 };
+    /// let coordinate2 = Cooordinate::D2 { x: 1.01, y: 2.02 };
+    /// assert!(coordinate1.equals_2d_with_tolerance(&coordinate2, 0.1));
+    ///
+    /// let coordinate1 = Cooordinate::D2 { x: 1.0, y: 2.0 };
+    /// let coordinate2 = Cooordinate::D2 { x: 3.0, y: 4.0 };
+    /// assert!(!coordinate1.equals_2d_with_tolerance(&coordinate2, 0.1));
+    /// 
+    /// let coordinate1 = Cooordinate::D3 { x: 1.0, y: 2.0, z: 3.0 };
+    /// let coordinate2 = Cooordinate::D3 { x: 1.0, y: 2.0, z: 3.1 };
+    /// assert!(coordinate1.equals_2d_with_tolerance(&coordinate2, 0.1));
+    /// ```
+    pub fn equals_2d_with_tolerance(&self, other: &Cooordinate, tolerance: f64) -> bool {
+        (self.x() - other.x()).abs() <= tolerance 
+        && (self.y() - other.y()).abs() <= tolerance
+    }
+
+    /// Checks if the 3D coordinates are equal.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - The first coordinate.
+    /// * `other` - The second coordinate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use geoms::coordinate::Cooordinate;
+    ///
+    /// let coordinate1 = Cooordinate::D3 { x: 1.0, y: 2.0, z: 3.0 };
+    /// let coordinate2 = Cooordinate::D3 { x: 1.0, y: 2.0, z: 3.0 };
+    /// assert!(coordinate1.equals_3d(&coordinate2));
+    ///
+    /// let coordinate1 = Cooordinate::D3 { x: 1.0, y: 2.0, z: 3.0 };
+    /// let coordinate2 = Cooordinate::D3 { x: 3.0, y: 4.0, z: 5.0 };
+    /// assert!(!coordinate1.equals_3d(&coordinate2));
+    /// ```
+    pub fn equals_3d(&self, other: &Cooordinate) -> bool {
+        self.x() == other.x() && self.y() == other.y() && self.z() == other.z()
     }
 
 }
