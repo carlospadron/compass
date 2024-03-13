761408cf67217d0c27631096a7dd6afb2b98a567
carlospadron <padron.ca@gmail.com>
2024-03-13 12:39:41 +0000

    make geometry into a trait, drafted a few abstract methods

---
+++ src/geometry/linearring.rs
@@ -0,0 +1,6 @@
+use crate::coordinate::Coordinate;
+
+//a representation of a linear ring
+pub struct LinearRing {
+    pub coordinates: Vec<Coordinate>
+}
--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,9 +1,9 @@
 use crate::coordinate::Coordinate;
 use crate::geometry::point::Point;
-use crate::geometry::linestring::LineString;
 
 pub mod point;
 pub mod linestring;
+pub mod linearring;
 pub mod polygon;
 pub mod multipoint;
 pub mod multilinestring;
@@ -12,12 +12,44 @@ pub mod geometrycollection;
 
 
 //set of possible geometries
-pub enum Geometry {
-    Point(Point),
-    LineString(LineString),
-    Polygon { exterior: Vec<Coordinate>, interior: Vec<Vec<Coordinate>>},
-    MultiPoint(Vec<Point>),
-    MultiLineString(Vec<LineString>),
-    MultiPolygon { polygons: Vec<(Vec<Coordinate>, Vec<Vec<Coordinate>>)>},
-    GeometryCollection { geometries: Vec<Geometry>}
-}
+pub trait Geometry {
+    //predicates
+    fn is_simple(&self) -> bool;
+    fn is_valid(&self) -> bool;
+    //accessors
+    fn area(&self) -> f64;
+    fn boundary(&self) -> &dyn Geometry;
+    fn coordinates(&self) -> Vec<Coordinate>;
+    fn envelope(&self) -> &dyn Geometry;
+    fn length(&self) -> f64;       
+    fn srid(&self) -> i32;
+    //constructive methods
+    fn buffer(&self, distance: f64) -> &dyn Geometry;
+    fn centroid(&self) -> Point;
+    fn difference(&self, other: &dyn Geometry) -> &dyn Geometry;
+    fn convex_hull(&self) -> &dyn Geometry;
+    fn intersection(&self, other: &dyn Geometry) -> &dyn Geometry;
+    fn simplify(&self, tolerance: f64) -> &dyn Geometry;    
+    fn sym_difference(&self, other: &dyn Geometry) -> &dyn Geometry;    
+    fn union(&self, other: &dyn Geometry) -> &dyn Geometry;
+    //measuring methods
+    fn distance(&self, other: &dyn Geometry) -> f64;
+    //distance relationships
+    fn is_within_distance(&self, other: &dyn Geometry, distance: f64) -> bool;
+    //topological relationships
+    fn contains(&self, other: &dyn Geometry) -> bool;
+    fn covers(&self, other: &dyn Geometry) -> bool;
+    fn covered_by(&self, other: &dyn Geometry) -> bool;
+    fn crosses(&self, other: &dyn Geometry) -> bool;
+    fn disjoint(&self, other: &dyn Geometry) -> bool;
+    fn intersects(&self, other: &dyn Geometry) -> bool;
+    fn overlaps(&self, other: &dyn Geometry) -> bool;
+    fn relate(&self, other: &dyn Geometry, matrix: &str) -> bool;
+    fn touches(&self, other: &dyn Geometry) -> bool;
+    fn within(&self, other: &dyn Geometry) -> bool;
+
+
+
+
+} 
+
