ef9d5d74c4d75d1b541cd093541d3edd169ba60d
carlospadron <padron.ca@gmail.com>
2024-03-13 12:59:28 +0000

    more methods and todos

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -20,6 +20,7 @@ pub trait Geometry {
     fn area(&self) -> f64;
     fn boundary(&self) -> &dyn Geometry;
     fn coordinates(&self) -> Vec<Coordinate>;
+    fn dimension(&self) -> i32;
     fn envelope(&self) -> &dyn Geometry;
     fn length(&self) -> f64;       
     fn srid(&self) -> i32;
@@ -29,6 +30,7 @@ pub trait Geometry {
     fn difference(&self, other: &dyn Geometry) -> &dyn Geometry;
     fn convex_hull(&self) -> &dyn Geometry;
     fn intersection(&self, other: &dyn Geometry) -> &dyn Geometry;
+    fn reverse(&self) -> &dyn Geometry;
     fn simplify(&self, tolerance: f64) -> &dyn Geometry;    
     fn sym_difference(&self, other: &dyn Geometry) -> &dyn Geometry;    
     fn union(&self, other: &dyn Geometry) -> &dyn Geometry;
@@ -42,6 +44,7 @@ pub trait Geometry {
     fn covered_by(&self, other: &dyn Geometry) -> bool;
     fn crosses(&self, other: &dyn Geometry) -> bool;
     fn disjoint(&self, other: &dyn Geometry) -> bool;
+    fn equals(&self, other: &dyn Geometry) -> bool;
     fn intersects(&self, other: &dyn Geometry) -> bool;
     fn overlaps(&self, other: &dyn Geometry) -> bool;
     fn relate(&self, other: &dyn Geometry, matrix: &str) -> bool;
