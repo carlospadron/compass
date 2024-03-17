721dd0074468af0f9ca6071415c26727d9d6887d
carlospadron <padron.ca@gmail.com>
2024-03-17 13:03:13 +0000

    more methods but using postgis as inspiration

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -15,7 +15,6 @@ pub mod geometrycollection;
 pub trait Geometry {
     //predicates
     fn is_simple(&self) -> bool;
-    fn is_valid(&self) -> bool;
     //accessors
     fn area(&self) -> f64;
     fn boundary(&self) -> &dyn Geometry;
@@ -23,22 +22,28 @@ pub trait Geometry {
     fn dimension(&self) -> i32;
     fn envelope(&self) -> &dyn Geometry;
     fn length(&self) -> f64;       
-    fn srid(&self) -> i32;
     //constructive methods
     fn buffer(&self, distance: f64) -> &dyn Geometry;
     fn centroid(&self) -> Point;
     fn difference(&self, other: &dyn Geometry) -> &dyn Geometry;
     fn convex_hull(&self) -> &dyn Geometry;
     fn intersection(&self, other: &dyn Geometry) -> &dyn Geometry;
-    fn normalize(&self) -> &dyn Geometry;
     fn reverse(&self) -> &dyn Geometry;
     fn simplify(&self, tolerance: f64) -> &dyn Geometry;    
     fn sym_difference(&self, other: &dyn Geometry) -> &dyn Geometry;    
     fn union(&self, other: &dyn Geometry) -> &dyn Geometry;
+    //editorial methods
+    fn normalize(&self) -> &dyn Geometry;
+    fn snap(&self, other: &dyn Geometry, tolerance: f64) -> &dyn Geometry;
+    fn snap_to_grid(&self, size: f64) -> &dyn Geometry;
     //measuring methods
     fn distance(&self, other: &dyn Geometry) -> f64;
     //distance relationships
     fn is_within_distance(&self, other: &dyn Geometry, distance: f64) -> bool;
+    //spatial reference system methods
+    fn set_srid(&self, srid: i32) -> &dyn Geometry;
+    fn srid(&self) -> i32;
+    fn transform(&self, srid: i32) -> &dyn Geometry;
     //topological relationships
     fn contains(&self, other: &dyn Geometry) -> bool;
     fn covers(&self, other: &dyn Geometry) -> bool;
@@ -51,6 +56,10 @@ pub trait Geometry {
     fn relate(&self, other: &dyn Geometry, matrix: &str) -> bool;
     fn touches(&self, other: &dyn Geometry) -> bool;
     fn within(&self, other: &dyn Geometry) -> bool;
+    //validation methods    
+    fn is_valid(&self) -> bool;
+    fn make_valid(&self) -> &dyn Geometry;
+
 
 
 
